a84ec7d5def2c0a005fc0bf3c42dd63d35cf3fac
karyuuin <31936393+karyuuin@users.noreply.github.com>
2018-11-26 16:27:08 +0300

    Create file.py
    создание массива

---
+++ file.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 10:07:24 2018
+
+@author: class
+"""
+import math
+import os
+import numpy as np
+import random
+
+a=[]
+p=1
+print("Введи размер массива ")
+n = int(input ())
+new = np.random.randint(1,100,n)
+a.append(new)
+print(a)     
